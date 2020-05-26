cb57b02fb212254fa4d5038124695836eadc3697
Marco Buratto <m.buratto@lumit.it>
2020-05-26 20:12:03 +0100

    Code cleanup, image

--- vbqemu.setup/bootstrap.sh
+++ vbqemu.setup/bootstrap.sh
@@ -15,9 +15,9 @@ function System()
 
     # Properties list.
     ACTION="$ACTION"
-	PROXY="$PROXY"
-	
-	SYSTEM_USERS_PASSWORD="password"
+    PROXY="$PROXY"
+
+    SYSTEM_USERS_PASSWORD="password"
 }
 
 # ##################################################################################################################################################
@@ -30,24 +30,24 @@ function System()
 function System_run()
 {
     if [ "$ACTION" == "install" ]; then
-	    if System_checkEnvironment; then
-			System_rootPasswordConfig "$SYSTEM_USERS_PASSWORD"
-			System_sshConfig
-			System_proxySet "$PROXY"
-			System_installDependencies
-			
-			System_installQemu
-			System_prepareQemuFiles
-			System_makeUp
-			
-			echo "System installation complete."
-		else
-			echo "A Debian Buster operating system is required for the installation. Aborting."
-			exit 1
-		fi
-	else
-		exit 1		
-	fi	
+        if System_checkEnvironment; then
+            System_rootPasswordConfig "$SYSTEM_USERS_PASSWORD"
+            System_sshConfig
+            System_proxySet "$PROXY"
+            System_installDependencies
+
+            System_installQemu
+            System_prepareQemuFiles
+            System_makeUp
+
+            echo "System installation complete."
+        else
+            echo "A Debian Buster operating system is required for the installation. Aborting."
+            exit 1
+        fi
+    else
+        exit 1
+    fi
 }
 
 # ##################################################################################################################################################
@@ -56,13 +56,13 @@ function System_run()
 
 function System_checkEnvironment()
 {
-	if [ -f /etc/os-release ]; then
-		if ! grep -q 'Debian GNU/Linux 10 (buster)' /etc/os-release; then
-			return 1
-		fi
-	else
-		return 1
-	fi
+    if [ -f /etc/os-release ]; then
+        if ! grep -q 'Debian GNU/Linux 10 (buster)' /etc/os-release; then
+            return 1
+        fi
+    else
+        return 1
+    fi
 
     return 0
 }
@@ -73,7 +73,7 @@ function System_rootPasswordConfig()
 {
     printf "\n* Setting a password for root...\n"
 
-	printf "$1\n$1" | passwd 
+    printf "$1\n$1" | passwd 
 }
 
 
@@ -81,10 +81,10 @@ function System_rootPasswordConfig()
 function System_sshConfig()
 {
     printf "\n* Enabling direct SSH with password auth for root...\n"
-   
-	sed -i 's/^PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+ 
+    sed -i 's/^PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
     sed -i 's/^#PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config	
-	systemctl restart ssh
+    systemctl restart ssh
 }
 
 
@@ -93,32 +93,33 @@ function System_proxySet()
 {
     printf "\n* Setting up system proxy...\n"
 
-	if ! grep -qi "http_proxy" /etc/environment; then
-		echo "http_proxy=$1" >> /etc/environment
-		echo "https_proxy=$1" >> /etc/environment
-	else	
-		sed -i "s|http_proxy=.*|http_proxy=$1|g" /etc/environment
-		sed -i "s|https_proxy=.*|https_proxy=$1|g" /etc/environment
-	fi
-	
-	export http_proxy=$1
-	export https_proxy=$1
+    if ! grep -qi "http_proxy" /etc/environment; then
+        echo "http_proxy=$1" >> /etc/environment
+        echo "https_proxy=$1" >> /etc/environment
+    else
+        sed -i "s|http_proxy=.*|http_proxy=$1|g" /etc/environment
+        sed -i "s|https_proxy=.*|https_proxy=$1|g" /etc/environment
+    fi
+
+    export http_proxy=$1
+    export https_proxy=$1
 }
 
-		
+
 
 function System_installDependencies()
 {
     printf "\n* Preparing the environment..."
 
-	sed -i 's/^deb cdrom/#deb cdrom/' /etc/apt/sources.list
+    sed -i 's/^deb cdrom/#deb cdrom/' /etc/apt/sources.list
     sed -i 's/main$/main contrib non-free/g' /etc/apt/sources.list	
-	
-	printf "\n* Installing system dependencies...\n"
-	apt install -y dos2unix git locales locales-all openssh-server unzip wget # base.
-	apt install -y gdm3 gnome-menus gnome-session gnome-shell gnome-terminal # gnome minimal.
-	
-	apt clean
+    apt update
+
+    printf "\n* Installing system dependencies...\n"
+    DEBIAN_FRONTEND=noninteractive apt install -y dos2unix git locales locales-all openssh-server unzip wget # base.
+    DEBIAN_FRONTEND=noninteractive apt install -y gdm3 gnome-menus gnome-session gnome-shell gnome-terminal # gnome minimal.
+
+    apt clean
 }
 
 
@@ -127,7 +128,7 @@ function System_installQemu()
 {
     printf "\n* Installing qemu from Bullseye (pinning)...\n"
 
-	cat >/etc/apt/preferences.d/qemu.pref<<EOF
+    cat >/etc/apt/preferences.d/qemu.pref<<EOF
 Package: *
 Pin: release n=buster
 Pin-Priority: 900
@@ -136,14 +137,14 @@ Package: qemu* gcc-10-base libgcc* libcrypt* libc-l10n locales* libc* libffi* li
 Pin: release n=bullseye
 Pin-Priority: 1000
 EOF
-	
-	cat >/etc/apt/sources.list.d/bullseye.list<<EOF
+
+    cat >/etc/apt/sources.list.d/bullseye.list<<EOF
 deb http://deb.debian.org/debian/ bullseye main contrib non-free
 deb-src http://deb.debian.org/debian/ bullseye main contrib non-free
 EOF
 
-	apt update
-    apt install -y qemu-system-aarch64
+    apt update
+    DEBIAN_FRONTEND=noninteractive apt install -y qemu-system-aarch64
 }
 
 
@@ -151,33 +152,33 @@ EOF
 function System_prepareQemuFiles()
 {
     printf "\n* Finalizing qemu environment...\n"
-	
+
     cd /tmp
     if [ -f hda.zip ]; then
-	    mkdir /home/vagrant/qemu
-		
-	    unzip hda.zip && mv hda.img /home/vagrant/qemu
-		rm -f hda.zip
-	else
-        exit 1	
-	fi
-	
-	if [ -f QEMU_EFI.fd ]; then
+        mkdir /home/vagrant/qemu
+
+        unzip hda.zip && mv hda.img /home/vagrant/qemu
+        rm -f hda.zip
+    else
+        exit 1
+    fi
+
+    if [ -f QEMU_EFI.fd ]; then
         mv QEMU_EFI.fd /home/vagrant/qemu
-	else
-        exit 1		
-	fi	
-	
+    else
+        exit 1
+    fi
+
     cat >/home/vagrant/qemu/run.sh<<EOF
 if [ -f hda.img ] && [ -f QEMU_EFI.fd ]; then
     qemu-system-aarch64 -m 2048 -M virt -cpu cortex-a72 -smp 2 -serial stdio -bios QEMU_EFI.fd -hda hda.img
 else
-	exit 1
-fi	
+    exit 1
+fi
 EOF
-	
-    chown vagrant:vagrant /home/vagrant/qemu/*	
-	chmod +x /home/vagrant/qemu/run.sh
+
+    chown vagrant:vagrant /home/vagrant/qemu/*
+    chmod +x /home/vagrant/qemu/run.sh
 }
 
 
@@ -185,12 +186,12 @@ EOF
 function System_makeUp()
 {
     printf "\n* Some makeup...\n"
-	
-	# Gnome autologin for user vagrant.
-	sed -i 's/^#  AutomaticLoginEnable.*/AutomaticLoginEnable = true/g' /etc/gdm3/daemon.conf
-	sed -i 's/^#  AutomaticLogin.*/AutomaticLogin = vagrant/g' /etc/gdm3/daemon.conf
-	
-	systemctl restart gdm3
+
+    # Gnome autologin for user vagrant.
+    sed -i 's/^#  AutomaticLoginEnable.*/AutomaticLoginEnable = true/g' /etc/gdm3/daemon.conf
+    sed -i 's/^#  AutomaticLogin.*/AutomaticLogin = vagrant/g' /etc/gdm3/daemon.conf
+
+    systemctl restart gdm3
 }
 
 # ##################################################################################################################################################
@@ -217,12 +218,12 @@ while [[ $# -gt 0 ]]; do
             shift
             shift
             ;;
-			
+
         --proxy)
             PROXY="$2"
             shift
             shift
-            ;;			
+            ;;
 
         *)
             shift
